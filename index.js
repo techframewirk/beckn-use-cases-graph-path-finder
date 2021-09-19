ebc8efb5e20d4bac04963abc3f8f2092e734ee7d
techframewirk <techframewirk@gmail.com>
2021-09-19 17:02:37 +0530

    bug fix

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ var combine_files = require('./graph').combine_files
 
 const start = () => {
     const jsons = fs.readdirSync('./inputs')
+    const processed = []
     for(const json of jsons) {
         if(json.endsWith('.json')) {
             console.log(`\nProcessing ${json}`)
             const contents = fs.readFileSync(`./inputs/${json}`, 'utf8')
             construct_tree(JSON.parse(contents))
+            processed.push(json)
         } else {
             console.log(`Skipped ${json}. Filename should end with .json`)
         }
     }
     console.log('\nCreating combined csv')
-    combine_files(jsons);
+    combine_files(processed);
     console.log('Process complete')
 }
 
